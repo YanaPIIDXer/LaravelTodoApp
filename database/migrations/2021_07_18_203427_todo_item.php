ff0a2d42af35d5f8aceebe70e5d8de761421ce05
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-07-19 05:58:08 +0900

    TodoItemのモデルとテーブルを作成

---
+++ database/migrations/2021_07_18_203427_todo_item.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TodoItem extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("todo_item", function(Blueprint $table)
+        {
+            $table->id()->autoIncrement();
+            $table->string("title", 64);
+            $table->string("body", 64);
+            $table->date("date");
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_bin';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("todo_item");
+    }
+}
