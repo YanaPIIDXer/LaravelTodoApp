e16c2679e4e8f4889848331919bf74c986f2afd0
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-07-19 06:30:01 +0900

    データを追加するようにした

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\TodoItem;
 
 class TodoController extends Controller
 {
@@ -13,6 +15,23 @@ class TodoController extends Controller
 
     public function add(Request $request)
     {
+        $validator = Validator::make($request->all(), 
+        [
+            'title' => 'required',
+            'body' => 'required',
+            'date' => 'required',
+        ]);
+
+        if (!$validator->fails())
+        {
+            $item = new TodoItem();
+
+            $item->title = $request->title;
+            $item->body = $request->body;
+            $item->date = $request->date;
+
+            $item->save();
+        }
         return redirect("/");
     }
 }
