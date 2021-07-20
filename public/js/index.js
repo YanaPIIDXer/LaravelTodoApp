86b802df066d9c72cebe6cc813d902799395e31f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-07-21 03:41:04 +0900

    constに変更

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,13 @@
 // 追加
 function add_item() {
-    var _token = $("#add_form [name=_token]").val();
-    var title = $("#add_form [name=title]").val();
-    var body = $("#add_form [name=body]").val();
-    var date = $("#add_form [name=date]").val();
+    const _token = $("#add_form [name=_token]").val();
+    const title = $("#add_form [name=title]").val();
+    const body = $("#add_form [name=body]").val();
+    const date = $("#add_form [name=date]").val();
 }
 
 // 消去
 function delete_item(id) {
-    var form_id = "#delete_form_" + id;
-    var _token = $(form_id + " [name=_token]").val();
+    const form_id = "#delete_form_" + id;
+    const _token = $(form_id + " [name=_token]").val();
 }
