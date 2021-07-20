47d0c2569463a381380812b57c3ad8a48cf8b0dc
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-07-21 03:46:30 +0900

    追加・削除のAPIを叩くようにした

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,30 @@ function add_item() {
     const title = $("#add_form [name=title]").val();
     const body = $("#add_form [name=body]").val();
     const date = $("#add_form [name=date]").val();
+    $.ajax("add", {
+        type: "post",
+        data: {
+            _token: _token,
+            title: title,
+            body: body,
+            date: date
+        }
+    }).done((data) => {
+        console.log(data);
+    });
 }
 
 // 消去
 function delete_item(id) {
     const form_id = "#delete_form_" + id;
     const _token = $(form_id + " [name=_token]").val();
+    $.ajax("delete", {
+        type: "post",
+        data: {
+            _token: _token,
+            id: id
+        }
+    }).done((data) => {
+        console.log(data);
+    })
 }
